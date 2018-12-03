c3a6e07d07e0dca5c77586396db83ea1fe3586ea
Sri Ramanujam <sricharan.ramanujam@gmail.com>
2018-12-02 23:50:16 -0500

    Improvements to day1 solutions
    I made a list into a set so that lookups could be O(1) instead of O(n)
    and it helps a lot.

--- 2018/day1/part2.py
+++ 2018/day1/part2.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python3
 
 if __name__ == '__main__':
-    seen = []
+    seen = set()
     current = 0
     num_iters = 0
-    frequencies = list(map(lambda x: int(x), open('input.txt', 'r').readlines()))
+    frequencies = [int(x) for x in open('input.txt', 'r').readlines()]
 
     while True:
         num_iters += 1
-        print("Iteration {}".format(num_iters))
         for freq in frequencies:
             current += freq
             if current in seen:
-                print(current)
+                print("The answer is {}".format(current))
                 exit(0)
             else:
-                seen.append(current)
-
+                seen.add(current)
