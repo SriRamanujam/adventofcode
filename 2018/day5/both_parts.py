5dde6328428da181a3a4914d0c3ecab7163fda06
Sri Ramanujam <sricharan.ramanujam@gmail.com>
2018-12-06 02:29:54 -0500

    Cleaned up day 5

--- 2018/day5/both_parts.py
+++ 2018/day5/both_parts.py
@@ -1,71 +1,53 @@
 #!/usr/bin/env python3
-import itertools
 
-def pairwise(iterable):
-    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
-    a, b = itertools.tee(iterable)
-    next(b, None)
-    return zip(a, b)
-
-def check_equal(list1, list2):
-    if len(list1) != len(list2):
-        return False
-
-    for i in range(len(list1)):
-        if list1[i] != list2[i]:
-            return False
-
-    return True
-
-
-def do_the_thing(poly_str):
-    poly_copy = poly_str
-
-    thing = list(poly_str)
-    temp = []
+def collapse_polymer(poly_str):
+    poly_chars_list = list(poly_str)
+    new_polymer_stack = []
     while True:
         try:
-            char1 = thing.pop()
+            char1 = poly_chars_list.pop()
         except IndexError:
-            return temp[::-1]
+            return new_polymer_stack[::-1]
         try:
-            char2 = thing.pop()
+            char2 = poly_chars_list.pop()
         except IndexError:
-            temp.append(char1)
-            return temp[::-1]
+            new_polymer_stack.append(char1)
+            return new_polymer_stack[::-1]
 
         if char1 == char2.swapcase():
             continue
         else:
-            temp.append(char1)
-            thing.append(char2)
+            new_polymer_stack.append(char1)
+            poly_chars_list.append(char2)
 
 
 
 if __name__ == '__main__':
     polymer = open('input.txt', 'r').read().strip()
 
-    def oh_jeez(what_this_do):
+    def perform_polymer_collapse(polymer_iteration):
         while True:
-            what_this_do_old = what_this_do
-            what_this_do = do_the_thing(what_this_do)
+            temp = polymer_iteration
+            polymer_iteration = collapse_polymer(polymer_iteration)
 
-            if what_this_do == what_this_do_old:
+            # if the collapse cannot find anything more to collapse, the two
+            # strings should be the same and we can break out and return
+            if polymer_iteration == temp:
                 break
-        return what_this_do
+        return polymer_iteration
 
-    what_this_do = oh_jeez(polymer)
-    print("Part1 answer: final length is {}".format(len(what_this_do)))
+    polymer_iteration = perform_polymer_collapse(polymer)
+    print("Part1 answer: final length is {}".format(len(polymer_iteration)))
 
     def do_part_2(polypoly, char):
         print("Computing result with letter {}".format(char))
         charlist = [char.lower(), char.upper()]
 
-        return len(oh_jeez(''.join(c for c in polypoly if c not in charlist)))
+        return len(perform_polymer_collapse(''.join(c for c in polypoly if c not in charlist)))
 
     results = {}
     for i in list('abcdefghijklmnopqrstuvwxyz'):
-        results[i] = do_part_2(''.join(what_this_do), i)
+        results[i] = do_part_2(''.join(polymer_iteration), i)
 
     min_letter = 50000
     what_letter = ''
